0e8d512ff85a9308ce91ebfd49d8f5f8df7b3418
Raúl Casanova <43655043+RaulC2803@users.noreply.github.com>
2020-08-27 14:05:39 -0500

    Update astar-grafo.py

--- astar-grafo.py
+++ astar-grafo.py
@@ -1,112 +1,130 @@
-# This class represent a graph
 class Graph:
-    # Initialize the class
+    
     def __init__(self, graph_dict=None, directed=True):
         self.graph_dict = graph_dict or {}
         self.directed = directed
         if not directed:
             self.make_undirected()
-    # Create an undirected graph by adding symmetric edges
+
+
     def make_undirected(self):
         for a in list(self.graph_dict.keys()):
             for (b, dist) in self.graph_dict[a].items():
                 self.graph_dict.setdefault(b, {})[a] = dist
-    # Add a link from A and B of given distance, and also add the inverse link if the graph is undirected
+
+                
     def connect(self, A, B, distance=1):
         self.graph_dict.setdefault(A, {})[B] = distance
         if not self.directed:
             self.graph_dict.setdefault(B, {})[A] = distance
-    # Get neighbors or a neighbor
+
+    #Obtenemos a los vecinos
     def get(self, a, b=None):
         links = self.graph_dict.setdefault(a, {})
         if b is None:
             return links
         else:
             return links.get(b)
-    # Return a list of nodes in the graph
+
+
+    #Retornamos una lista de nodos en el grafo
     def nodes(self):
         s1 = set([k for k in self.graph_dict.keys()])
         s2 = set([k2 for v in self.graph_dict.values() for k2, v2 in v.items()])
         nodes = s1.union(s2)
         return list(nodes)
-# This class represent a node
+
+
 class Node:
-    # Initialize the class
+
+
     def __init__(self, name:str, parent:str):
         self.name = name
         self.parent = parent
-        self.g = 0 # Distance to start node
-        self.h = 0 # Distance to goal node
-        self.f = 0 # Total cost
-    # Compare nodes
+        self.g = 0 # Distancia acumulada hasta el actual nodo
+        self.h = 0 # Distancia Manhatam
+        self.f = 0 # Total de costo
+
+
+    # Operador de == 
     def __eq__(self, other):
         return self.name == other.name
-    # Sort nodes
+
+    # Ordenamos ascendentemente
     def __lt__(self, other):
          return self.f < other.f
-    # Print node
+
+
+    # Imprimimos el nodo
     def __repr__(self):
         return ('({0},{1})'.format(self.position, self.f))
-# A* search
+
+# A* start
 def astar_search(graph, heuristics, start, end):
     
-    # Create lists for open nodes and closed nodes
+    #Creamos la OpenList y la CloseList
     open = []
     closed = []
-    # Create a start node and an goal node
+
     start_node = Node(start, None)
     goal_node = Node(end, None)
-    # Add the start node
+
+    # Añadimos el primer nodo a la OpenList
     open.append(start_node)
     
-    # Loop until the open list is empty
+    # Loop hasta que quede vacía la OpenList
     while len(open) > 0:
-        # Sort the open list to get the node with the lowest cost first
+
         open.sort()
-        # Get the node with the lowest cost
+
+        #Obtenemos el nodo con menos costo
         current_node = open.pop(0)
-        # Add the current node to the closed list
+
+        #Al evaluarlo lo ponemos en la CloseList
         closed.append(current_node)
-        
-        # Check if we have reached the goal, return the path
+       
+       #si el nodo actual es igual al nodo meta 
         if current_node == goal_node:
             path = []
+
             while current_node != start_node:
-                path.append(current_node.name + ': ' + str(current_node.g))
+                path.append(current_node.name + ': (g = ' + str(current_node.g) + ', h = ' + str(current_node.h) + ', f = ' + str(current_node.f) + ')')
                 current_node = current_node.parent
-            path.append(start_node.name + ': ' + str(start_node.g))
-            # Return reversed path
+            path.append(start_node.name + ': (g = ' + str(start_node.g) + ', h = ' + str(start_node.h) + ', f = '+ str(start_node.f) + ')')
+
             return path[::-1]
-        # Get neighbours
+
+        #Obtiene a los nodos hijos del nodo actual (vecinos)
         neighbors = graph.get(current_node.name)
-        # Loop neighbors
+
         for key, value in neighbors.items():
-            # Create a neighbor node
+
             neighbor = Node(key, current_node)
-            # Check if the neighbor is in the closed list
+
+            # Si el nodo está en la CloseList no se evalua
             if(neighbor in closed):
                 continue
-            # Calculate full path cost
+
+            # Aquí calculamos los g, h y f del nodo vecino
             neighbor.g = current_node.g + graph.get(current_node.name, neighbor.name)
             neighbor.h = heuristics.get(neighbor.name)
             neighbor.f = neighbor.g + neighbor.h
-            # Check if neighbor is in open list and if it has a lower f value
+
+            #Si el f es menor al de todos los nodos que están en la lista de abierto no se le agrega a la OpenList
             if(add_to_open(open, neighbor) == True):
-                # Everything is green, add neighbor to open list
                 open.append(neighbor)
-    # Return None, no path is found
+
     return None
-# Check if a neighbor should be added to open list
+
 def add_to_open(open, neighbor):
     for node in open:
         if (neighbor == node and neighbor.f > node.f):
             return False
     return True
-# The main entry point for this module
+
 def main():
-    # Create a graph
     graph = Graph()
-    # Create graph connections (Actual distance)
+
     graph.connect('Arad', 'Zerind', 75)
     graph.connect('Arad', 'Sibiu', 140)
     graph.connect('Arad', 'Timisoara', 118)
@@ -140,9 +158,7 @@ def main():
     graph.connect('Iasi','Neamt', 87)
     
 
-    # Make graph undirected, create symmetric connections
     graph.make_undirected()
-    # Create heuristics (straight-line distance, air-travel distance)
     heuristics = {}
     heuristics['Arad'] = 366
     heuristics['Bucharest'] = 0
@@ -164,9 +180,11 @@ def main():
     heuristics['Urziceni'] = 80
     heuristics['Vaslui'] = 199
     heuristics['Zerind'] = 374
-    # Run the search algorithm
+
     path = astar_search(graph, heuristics, 'Timisoara', 'Bucharest')
+
     print(path)
     print()
-# Tell python to run main method
+
+
 if __name__ == "__main__": main()
